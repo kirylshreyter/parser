01984d13c94c84588f636286f7c0496722aaad4a
Admin\kiryl <kirylshreyter@gmai;.com>
2017-07-24 11:18:31 +0300

    New change.

--- src/com/kirylshreyter/parser/Main.java
+++ src/com/kirylshreyter/parser/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		Parser parser = ParserFactory.getParser("e:/java/workspaces/Parser/sample.json");
-		parser.parse("e:/java/workspaces/Parser/sample.json");
+		parser.parse("e:/java/workspaces/parser/sample.json");
 
 	}
 
